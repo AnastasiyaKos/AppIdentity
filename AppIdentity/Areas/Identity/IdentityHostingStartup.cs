99d7e1bf8cb5ee91b0cde96f06897da31b675585
Nastya Kosukha <47366500+AnastasiyaKos@users.noreply.github.com>
2020-04-17 21:25:53 +0300

    Initial commit

---
+++ AppIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AppIdentity.Areas.Identity.Data;
+using AppIdentity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+[assembly: HostingStartup(typeof(AppIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace AppIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AppUsersDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AppUsersDbContextConnection")));
+
+                services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AppUsersDbContext>();
+            });
+        }
+    }
+}
---
+++ AppIdentity/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AppIdentity.Models;
+using AppIdentity.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AppIdentity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AppUsersDbContext _appUsersDbContext;
+
+        public HomeController(ILogger<HomeController> logger, AppUsersDbContext appUsersDbContext)
+        {
+            _logger = logger;
+            _appUsersDbContext = appUsersDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return RedirectToPage("/Account/Login", new { area = "Identity" });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Users()
+        {
+            ViewBag.Users = _appUsersDbContext.Users;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
